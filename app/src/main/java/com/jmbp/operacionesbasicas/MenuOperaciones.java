e7c4ca3a3fc12d2a76aad64a8a49d2937a24fc00
MiguelBrucil <132540041+MiguelBrucil@users.noreply.github.com>
2023-11-18 21:23:59 -0500

    Programacion de variables en la resta

---
+++ app/src/main/java/com/jmbp/operacionesbasicas/MenuOperaciones.java
@@ -0,0 +1,14 @@
+package com.jmbp.operacionesbasicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuOperaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_operaciones);
+    }
+}
--- app/src/main/java/com/jmbp/operacionesbasicas/Resta.java
+++ app/src/main/java/com/jmbp/operacionesbasicas/Resta.java
@@ -3,12 +3,26 @@ package com.jmbp.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Resta extends AppCompatActivity {
-
+    EditText ednumRes1,ednumRes2;
+    TextView edResultadoRes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resta);
+        ednumRes1=findViewById(R.id.num1Resta);
+        ednumRes2=findViewById(R.id.num2Resta);
+        edResultadoRes=findViewById(R.id.resResta);
+    }
+    public void calcularResultado(View v){
+        double num1=Double.parseDouble(ednumRes1.getText().toString());
+        int num2= Integer.parseInt((ednumRes2.getText().toString()));
+        double resultado=num1-num2;
+        edResultadoRes.setText(resultado+"");
+
     }
 }
