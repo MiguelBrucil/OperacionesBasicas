0075102e4ab52b25028121080a210acb2ddedc13
MiguelBrucil <132540041+MiguelBrucil@users.noreply.github.com>
2023-11-18 21:30:33 -0500

    Programacion de la parte de la divison con sus variables

--- app/src/main/java/com/jmbp/operacionesbasicas/Division.java
+++ app/src/main/java/com/jmbp/operacionesbasicas/Division.java
@@ -3,12 +3,26 @@ package com.jmbp.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Division extends AppCompatActivity {
-
+    EditText ednum1Divi,ednum2Divi;
+    TextView edResultadoDivi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_division);
+        ednum1Divi=findViewById(R.id.num1Divi);
+        ednum2Divi=findViewById(R.id.num2Divi);
+        edResultadoDivi=findViewById(R.id.resDivi);
+    }
+    public void calcularResultado(View v){
+        double num1=Double.parseDouble(ednum1Divi.getText().toString());
+        int num2= Integer.parseInt((ednum2Divi.getText().toString()));
+        double resultado=num1/num2;
+        edResultadoDivi.setText(resultado+"");
+
     }
 }
